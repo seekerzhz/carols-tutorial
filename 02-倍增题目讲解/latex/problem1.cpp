409b7a773b54d88a7d13b4c50cf1075741690b35
SeekerHaron <89367909+seekerzhz@users.noreply.github.com>
2023-02-14 13:32:40 +0800

    补全了第一次课的课件，开始写第二次课题解

---
+++ 02-倍增题目讲解/latex/problem1.cpp
@@ -0,0 +1,36 @@
+#include<bits/stdc++.h>
+using namespace std;
+const int MAXN=1e6+10;
+inline int read() {
+	char c=getchar();
+	int x=0,f=1;
+	while(c<'0'||c>'9') {
+		if(c=='-')f=-1;
+		c=getchar();
+	}
+	while(c>='0'&&c<='9') {
+		x=x*10+c-'0';
+		c=getchar();
+	}
+	return x*f;
+}
+int Max[MAXN][21];
+int Query(int l,int r) {
+	int k=log2(r-l+1);
+	return max(Max[l][k],Max[r-(1<<k)+1][k]);
+}
+int main() {
+	int N=read(),M=read();
+	for(int i=1; i<=N; i++)
+		Max[i][0]=read();
+	for(int j=1; j<=21; j++) {
+		for(int i=1; i+(1<<j)-1<=N; i++)
+			Max[i][j]=max(Max[i][j-1],Max[i+(1<<(j-1))][j-1]);
+
+	}
+	for(int i=1; i<=M; i++) {
+		int l=read(),r=read();
+		printf("%d\n",Query(l,r));
+	}
+	return 0;
+}
--- README.md
+++ README.md
@@ -1,3 +1,6 @@
 # tutorial
-上课用课件
+上课用课件，所有以pdf结尾的文件都是资料
+
 QQ；1213954601
+
+email:zhaohanzheng@yeah.net
